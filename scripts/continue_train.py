f1e47b8e78fd9f90abe7cace1f58fc36e0fcedd4
UltralyticsAssistant <web@ultralytics.com>
2026-07-04 08:35:06 +0000

    Auto-format by https://ultralytics.com/actions

--- scripts/continue_train.py
+++ scripts/continue_train.py
@@ -1,11 +1,10 @@
-from ultralytics import YOLO
 from multiprocessing import freeze_support
 
-if __name__ == '__main__':
+from ultralytics import YOLO
+
+if __name__ == "__main__":
     freeze_support()
 
-    model = YOLO(r'C:\E\github\ultralytics\runs\obb\train10\weights\last.pt')
+    model = YOLO(r"C:\E\github\ultralytics\runs\obb\train10\weights\last.pt")
 
-    model.train(
-        resume=True
-    )
+    model.train(resume=True)
